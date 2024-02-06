9e215241837dabf3e23dd9a6646065ee6a2a2d3e
jonathan <aungzayar1352000@gmail.com>
2024-02-07 00:23:39 +0630

    add store folder for posts

--- data/util.js
+++ data/util.js
@@ -1,12 +1,12 @@
 const fs = require("node:fs/promises");
 
 async function readData() {
-  const data = await fs.readFile("posts.json", "utf8");
+  const data = await fs.readFile("./store/posts.json", "utf8");
   return JSON.parse(data);
 }
 
 async function writeData(data) {
-  await fs.writeFile("posts.json", JSON.stringify(data));
+  await fs.writeFile("./store/posts.json", JSON.stringify(data));
 }
 
 exports.readData = readData;
